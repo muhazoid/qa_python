b796a0b8e951c308c6d241c4e7976c90c0efab9d
Aleksey Chikichev <Agentlich2012@gmail.com>
2026-02-26 19:16:26 +0700

    popravlen perviy test

--- tests.py
+++ tests.py
@@ -18,7 +18,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
